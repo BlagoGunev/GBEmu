50845ca20c7ad7097e8f2e3537b297ee210c5437
Blago Gunev <bgunev@gmail.com>
2024-11-22 12:03:30 +0100

    Logging changes

--- code/lib/cpu.c
+++ code/lib/cpu.c
@@ -34,9 +34,17 @@ bool cpu_step() {
         fetch_instruction();
         fetch_data();
 
-        printf("Executing: %02X,   PC: %04X, Data (%02X %02X), Fetched: %04X\n\tA: %02X, F: %02X, BC: %02X%02X, DE: %02X%02X, HL: %02X%02X\n", 
-            ctx.cur_opcode, pc, bus_read(pc+1), bus_read(pc+2), 
-            ctx.fetched_data, ctx.regs.a, ctx.regs.f, ctx.regs.b, ctx.regs.c, ctx.regs.d, ctx.regs.e, ctx.regs.h, ctx.regs.l);
+        char flags[16];
+        sprintf(flags, "%c%c%c%c", 
+            ctx.regs.f & (1 << 7) ? 'Z' : '-',
+            ctx.regs.f & (1 << 6) ? 'N' : '-',
+            ctx.regs.f & (1 << 5) ? 'H' : '-',
+            ctx.regs.f & (1 << 4) ? 'C' : '-'
+        );
+
+        printf("Ticks: %lld\nExecuting: %02X,   PC: %04X, Data (%02X %02X), Fetched: %04X\n\tA: %02X, F: %s, BC: %02X%02X, DE: %02X%02X, HL: %02X%02X\n", 
+            emu_get_context()->ticks, ctx.cur_opcode, pc, bus_read(pc+1), bus_read(pc+2), 
+            ctx.fetched_data, ctx.regs.a, flags, ctx.regs.b, ctx.regs.c, ctx.regs.d, ctx.regs.e, ctx.regs.h, ctx.regs.l);
         
         if (ctx.cur_inst == NULL) {
             printf("Unknown instruction: %02X\n", ctx.cur_opcode);
