0e573ffe12e07214cb69844f14b90a8bb9d81da6
Blago Gunev <bgunev@gmail.com>
2025-01-31 21:02:45 +0100

    Add window init

--- code/lib/emu.c
+++ code/lib/emu.c
@@ -2,8 +2,9 @@
 #include <emu.h>
 #include <cart.h>
 #include <cpu.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#include <ui.h>
+#include <pthread.h>
+#include <unistd.h>
 
 /* 
   Emu components:
@@ -22,8 +23,28 @@ emu_context *emu_get_context() {
     return &ctx;
 }
 
-void delay(u32 ms) {
-    SDL_Delay(ms);
+void *cpu_run(void *p) {
+    cpu_init();
+
+    ctx.running = true;
+    ctx.paused = false;
+    ctx.ticks = 0;
+
+    while(ctx.running) {
+        if (ctx.paused) {
+            delay(10);
+            continue;
+        }
+
+        if (!cpu_step()) {
+            printf("CPU Stopped\n");
+            return 0;
+        }
+
+        ctx.ticks++;
+    }
+
+    return 0;
 }
 
 int emu_run(int argc, char **argv) {
@@ -39,29 +60,18 @@ int emu_run(int argc, char **argv) {
 
     printf("Cart loaded..\n");
 
-    SDL_Init(SDL_INIT_VIDEO);
-    printf("SDL INIT\n");
-    TTF_Init();
-    printf("TTF INIT\n");
+    ui_init();
 
-    cpu_init();
-    
-    ctx.running = true;
-    ctx.paused = false;
-    ctx.ticks = 0;
+    pthread_t t1;
 
-    while(ctx.running) {
-        if (ctx.paused) {
-            delay(10);
-            continue;
-        }
+    if (pthread_create(&t1, NULL, cpu_run, NULL)) {
+        fprintf(stderr, "Couldn't create CPU thread. Aborting...\n");
+        return -1;
+    }
 
-        if (!cpu_step()) {
-            printf("CPU Stopped\n");
-            return -3;
-        }
-
-        ctx.ticks++;
+    while (!ctx.die) {
+        usleep(1000);
+        ui_handle_events();
     }
 
     return 0;
---
+++ code/lib/ui.c
@@ -0,0 +1,33 @@
+#include <ui.h>
+#include <emu.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+SDL_Window *sdlWindow;
+SDL_Renderer *sdlRenderer;
+SDL_Texture *sdlTexture;
+SDL_Surface *screen;
+
+void ui_init() {
+
+    SDL_Init(SDL_INIT_VIDEO);
+    printf("SDL INIT\n");
+    TTF_Init();
+    printf("TTF INIT\n");
+
+    SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &sdlWindow, &sdlRenderer);
+}
+
+void ui_handle_events() {
+    SDL_Event e;
+    while (SDL_PollEvent(&e) > 0) {
+        if (e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_CLOSE) {
+            emu_get_context()->die = true;
+        }
+    }
+}
+
+void delay(u32 ms) {
+    SDL_Delay(ms);
+}
